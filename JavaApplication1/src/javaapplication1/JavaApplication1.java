10e0ee77dc50755b5e93c14ed59f3de868f59b31
Hibaroid <60915724+Hibaroid@users.noreply.github.com>
2020-02-11 12:01:53 +0400

    Add files via upload

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import newpackage.*;
+/**
+ *
+ * @author Forminor
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { 
+        // TODO code application logic here
+       NewJFrame Form1 = new NewJFrame();
+       Form1.main();     
+    }
+    
+}
